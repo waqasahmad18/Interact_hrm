4bc66f41660cd56ad284fdddc100cb7fedcb61f2
waqas rafique <vickyksr2218@gmail.com>
2026-02-05 19:39:44 +0500

    Fix manage breaks page module

---
+++ app/admin/manage-breaks/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import LayoutDashboard from "../../layout-dashboard";
+
+export default function ManageBreaksPage() {
+	return (
+		<LayoutDashboard>
+			<div style={{ padding: 24 }}>
+				<h1 style={{ fontSize: "1.5rem", fontWeight: 700 }}>Manage Breaks</h1>
+				<p style={{ marginTop: 8, color: "#718096" }}>Coming soon.</p>
+			</div>
+		</LayoutDashboard>
+	);
+}
